a48113c4a379830f41765ae98f50e9a304032d9e
Stella Biderman <stellabiderman@gmail.com>
2021-10-14 15:04:28 -0400

    Update megatron.py

--- lm_eval/models/megatron.py
+++ lm_eval/models/megatron.py
@@ -1,18 +1,105 @@
-import transformers
+from abc import ABC, abstractmethod
+import numpy as np
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
-from lm_eval.base import LM, TokenizedLM
-from lm_eval import utils
 from tqdm import tqdm
-import numpy as np
-from abc import ABC, abstractmethod
+import transformers
 from typing import Iterable
 
-import gpt_neox
-from gpt_neox.megatron.text_generation_utils import generate_samples_from_prompt
+from lm_eval import utils
+from lm_eval.base import LM, TokenizedLM, TorchLM
+
+from megatron import get_tokenizer
+from megatron.checkpointing import load_checkpoint
+from megatron.model import GPTModel
+from megatron.training import get_model
+from megatron.text_generation_utils import generate_samples_eval
 
 class MegatronLM(TorchLM):
+
+    def __init__(self, device='cuda', batch_size=1, **kwargs):
+      
+        super().__init__()
+        
+        assert isinstance(device, str)
+        assert isinstance(batch_size, int)
+        
+        get_model(lambda x: GPTModel(num_tokentypes=0, parallel_output=False, pre_process=True, post_process=True))
+
+        if args.load is not None:
+            _ = load_checkpoint(model, None, None) 
+            
+        assert len(model) == 1, "Above condition should have caught this"
+        model = model[0]
+        
+        # Generate samples.
+        generate_and_write_samples_unconditional(model)
+
+        if device:
+            self.device = torch.device(device)
+        else:
+            self.device = torch.device('cuda') if torch.cuda.is_available() else torch.device('cpu')
+
+        self.gpt2 = transformers.AutoModelForCausalLM.from_pretrained(pretrained, revision=revision +("/" + subfolder if subfolder is not None else "")).to(self.device)
+        self.gpt2.eval()
+
+        self.tokenizer = neox_args.tokenizer
+
+        self.vocab_size = self.tokenizer.vocab_size
+        self.eot_token_id = self.tokenizer.eos_token_id # we use EOT because end of *text* is more accurate for what we're doing than end of *sentence*
+        self.max_gen_toks = 256
+
+        # multithreading and batching
+        gpus = torch.cuda.device_count()
+        batch_size_per_gpu = batch_size # todo: adaptive batch size
+
+        # TODO: fix multi-gpu
+        self.batch_size = batch_size_per_gpu# * gpus
+
+        # TODO: fix multi-gpu
+        # if gpus > 1:
+        #     self.gpt2 = nn.DataParallel(self.gpt2)
+    
+    def tok_encode(self, string: str):
+        return self.tokenizer.encode(string, add_special_tokens=False)
+    
+    def tok_decode(self, tokens):
+        return self.tokenizer.decode(tokens)
+
+    def _model_call(self, inps):
+        """
+        inps: a torch tensor of shape [batch, sequence]
+        the size of sequence may vary from call to call
+
+        returns: a torch tensor of shape [batch, sequence, vocab] with the
+        logits retuned from the model
+        """
+
+        data_wrapped = iter([{'text': F.pad(inps, pad=(0, 1))}])
+        if self.neox_args.is_pipe_parallel:
+            # need these flags to stop deepspeed from hanging
+            self.model.first_output_send = True
+            self.model.pipe_recv_buf = None
+        _, logits = self._forward_step_fn(model=self.model, data_iterator=data_wrapped)
+        return logits
+    
+    def _model_generate(self, context, max_length, eos_token_id):
+        return generate_samples_from_prompt(
+            neox_args=neox_args, 
+            model = self.model,
+            text=context,
+            eos_token_id = eos_token_id,
+            maximum_tokens = max_lenth
+            recompute = neox_args.recompute, 
+            temperature = neox_args.temperature,
+            top_k = neox_args.top_k, 
+            top_p = neox_args.top_p,
+            stop_tokens = neox_args.stop_tokens
+        )
+
+'''
+class GPTNeoXLM(TorchLM):
 
     def __init__(self, neox_args, device='cuda', batch_size=1):
       
@@ -83,3 +170,4 @@ class MegatronLM(TorchLM):
             top_p = neox_args.top_p,
             stop_tokens = neox_args.stop_tokens
         )
+'''
